c8299482c59b26ebbf3ef9a412db79bfd5136351
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2023-09-05 16:37:29 -0400

    Make sysimage BAT file add PackageCompiler
    Typically won't be installed

--- test/make_AMES_sysimage.bat
+++ test/make_AMES_sysimage.bat
@@ -1,2 +1,3 @@
+julia -e "using Pkg; Pkg.add(\"PackageCompiler\")"
 julia --trace-compile=AMES_precompile.jl AMES-run.jl
 julia -e "using PackageCompiler; PackageCompiler.create_sysimage([\"AMES\"]; sysimage_path=\"AMES-sysimage.so\", precompile_statements_file=\"AMES_precompile.jl\")"

aab3a11677b10f1f6ddd39f90762a87479a8b5d9
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2025-01-17 16:18:09 +0000

    Added unit tests for some AMESlib functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,28 @@
 using AMES
 using Test
 
+include("../src/AMESlib.jl")
+using .AMESlib
+
 curr_working_dir = pwd()
 cd(@__DIR__)
+
+@testset "AMESlib" begin
+	# char_index_to_int
+	@test AMESlib.char_index_to_int("W") == 23
+	@test AMESlib.char_index_to_int("CB") == 80
+	@test AMESlib.char_index_to_int("ABP") == 744
+
+	# excel_ref_to_rowcol
+	@test AMESlib.excel_ref_to_rowcol("A276") == [276,1]
+	@test AMESlib.excel_ref_to_rowcol("AB5") == [5,28]
+	@test AMESlib.excel_ref_to_rowcol("WM76") == [76,611]
+
+	# haskeyvalue
+	@test AMESlib.haskeyvalue(Dict("a" => 1, "b" => 2), "a")
+	@test !AMESlib.haskeyvalue(Dict("a" => 1, "b" => 2), "c")
+	@test !AMESlib.haskeyvalue(Dict("a" => nothing, "b" => 2), "a")
+end
 
 @testset "AMES.run" begin
 	@test AMES.run("AMES_params.yml",
